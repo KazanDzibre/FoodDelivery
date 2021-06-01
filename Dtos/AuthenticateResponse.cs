a4d9ed3263a938b265b816820cf050e28146c691
Aleksandar <aleksandar.skrobic@lilly021.com>
2021-06-01 12:18:55 +0200

    dodao neke pakete za jwt

--- Dtos/AuthenticateResponse.cs
+++ Dtos/AuthenticateResponse.cs
@@ -1,8 +1,18 @@
+using FoodDelivery.Models;
+
 namespace FoodDelivery.Dtos
 {
 	public class AuthenticateResponse
 	{
 		public string UserName { get; set; }
 		public string Type { get; set; }
+		public string Token { get; set; }
+
+		public AuthenticateResponse(User user, string token)
+		{
+			UserName = user.UserName;
+			Type = user.Type;
+			Token = token;
+		}
 	}
 }
